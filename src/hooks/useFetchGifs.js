b1277839e539c4020bd4d430af8b9cac83a97b93
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-01-10 01:17:26 +0100

    agregar animaciones a varios componentes con la libreria Animate.css

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -18,7 +18,7 @@ export const useFetchGifs = (category) => {
                         data: imgs,
                         loading: false,
                     })
-                },1500)
+                },3000)
 
             })
 
