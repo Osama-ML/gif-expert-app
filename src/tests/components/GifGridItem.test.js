e42f02941e9d4024ad66a00f10e9b6e4e56768e8
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-01-12 13:54:12 +0100

    Añadir test unitarios al componente GifGridItem para asegurar su funcionamiento

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,13 +4,32 @@ import { GifGridItem } from "../../components/GifGridItem";
 
 describe('Pruebas en <GifGridItem/>', () => {
 
-    const titulo = 'Título';
-    const url = 'https://localhost/algo.jpeg';
+    const titleTest = 'Título';
+    const urlTest = 'https://localhost/algo.jpeg';
+    const wrapper = shallow(<GifGridItem title={titleTest} url={urlTest}/>)
 
-    test('Debe de mostrar el contenido correctamente', () => {
-        
-        const wrapper = shallow(<GifGridItem title={titulo} url={url}/>)
+    test('Debe de mostrar el contenido correctamente', () => {    
         expect(wrapper).toMatchSnapshot();
     })
     
+    test('Debe de tener un parrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(titleTest);
+    })
+    
+    test('Debe de tener la imagen igual a las propiedades', () => {
+        const img = wrapper.find('img');
+        const {src, alt} = img.props();
+        expect(alt).toBe(titleTest);
+        expect(src).toBe(urlTest);
+    })
+    
+
+    test('Debe tener la clase animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const classTest = div.prop('className');
+        expect(classTest.includes('animate__fadeIn')).toBe(true);
+    })
+    
+
 })
