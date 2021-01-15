cf28b97484b02f60df74ba28055df82ceb13bc59
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-01-15 18:48:13 +0100

    Actualización de las pruebas GifGrid más robusto

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useFetchGifs } from '../hooks/useFetchGifs';
+import PropTypes from 'prop-types';
 
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 
@@ -28,4 +29,8 @@ export const GifGrid = ({category}) => {
             </div>
         </>
     )
+}
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired,
 }
