4d244715a0c77587a12146df35151263919e5f62
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-01-15 19:32:54 +0100

    Pruebas en GifExpertApp

---
+++ src/tests/GifExpertApp.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import GifExpertApp from '../GifExpertApp';
+
+import { shallow } from 'enzyme';
+
+describe('Pruebas en GifExpertApp', () => {
+
+    const wrapper = shallow(<GifExpertApp/>)
+
+    test('Debe tener la misma estructura que el snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+  
+    test('debe de mostrar una liste de categorias', () => {
+        
+        const categories = ['One punch','Dragonball'];
+        const wrapper = shallow(<GifExpertApp defaultCategories= {categories}/>)
+
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('GifGrid').length).toBe(categories.length)
+    })
+    
+
+})
