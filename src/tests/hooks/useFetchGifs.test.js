458e7645ba730b50a5013fc14e7f27bf798b6ddc
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-01-20 00:02:38 +0000

    Testear la llamada de useFetchGifs y comprobar su buen funcionamiento

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,14 +3,26 @@ import {renderHook} from '@testing-library/react-hooks'
 
 
 describe('Pruebas en useFetchGifs', () => {
-    test('debe de retornar el estado inicial', () => {
+    test('debe de retornar el estado inicial', async() => {
         
-        const {result} = renderHook( () => useFetchGifs('One punch') )
+        const {result, waitForNextUpdate} = renderHook( () => useFetchGifs('One punch') )
         const {data, loading} = result.current;
 
+        await waitForNextUpdate();
         expect(data).toEqual([]);
         expect(loading).toBe(true);
 
     })
     
+    test('debe de retornar un arreglo de imgs y el loading en false', async () => {
+        
+        const {result, waitForNextUpdate} = renderHook( () => useFetchGifs('One punch') );
+        await waitForNextUpdate();
+        const {data, loading} = result.current;
+
+        expect(data.length).toEqual(10);
+        expect(loading).toBe(false);
+    })
+    
+
 })
